67513f58e5cfaa21364f5c1b63b88694fd20c371
Suyash Kumar <suyash.kumar@duke.edu>
2015-04-21 00:35:27 -0400

    Added Win/Loss Overview, structural changes

--- app/static/core.js
+++ app/static/core.js
@@ -2,12 +2,14 @@ var bumpapp=angular.module('bumpapp',[]);
 
 function mainController($scope, $http){
 
-$http.get('/api/bumpers').success(
-			function(data){ //data is json data response
-			$scope.bumpers=data;
-			//console.log(data);
+	$http.get('/api/bumpers').success(
+				function(data){ //data is json data response
+				$scope.bumpers=data;
+				//console.log(data);
 
-		});
+			});
+	$scope.playerWins="-";
+	$scope.playerLosses="-";
 
 	$scope.getBumpers=function(){
 		console.log("Called");
@@ -26,11 +28,18 @@ $http.get('/api/bumpers').success(
 				$scope.dates=data['date'];
 				$scope.wins=data['win'];
 				$scope.both=[];
-				
+				var mySum=0;
 				for(i=0;i<data['win'].length;i++){
-					//$scope.both.push([data['win'][i],data['date'][i]]);
+					// Store win and date date in "both" array. Easier for front end vis.
+					// Each element is an array representing one game. The first element is 
+					// date, second is win status. 
 					$scope.both[i]=[data['date'][i], data['win'][i]];
+					// Keep track of wins
+					mySum+=data['win'][i];
+
 				}
+				$scope.playerWins=mySum;
+				$scope.playerLosses=data['win'].length-mySum;
 				console.log("Now")
 				console.log($scope.wins);
 
