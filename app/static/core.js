f684b5f5a8cc1159305fbea6e60ac6444e6793d7
Suyash Kumar <suyash.kumar@duke.edu>
2015-04-21 01:24:17 -0400

    Added simple nvd3.js Pie Chart
    Started integrating nvd3.js charting

--- app/static/core.js
+++ app/static/core.js
@@ -1,5 +1,6 @@
 var bumpapp=angular.module('bumpapp',[]);
 
+
 function mainController($scope, $http){
 
 	$http.get('/api/bumpers').success(
@@ -8,8 +9,75 @@ function mainController($scope, $http){
 				//console.log(data);
 
 			});
+
 	$scope.playerWins="-";
 	$scope.playerLosses="-";
+	$scope.options = {
+            chart: {
+                type: 'discreteBarChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 60,
+                    left: 55
+                },
+                x: function(d){return d.label;},
+                y: function(d){return d.value;},
+                showValues: true,
+                valueFormat: function(d){
+                    return d3.format(',.4f')(d);
+                },
+                transitionDuration: 500,
+                xAxis: {
+                    axisLabel: 'X Axis'
+                },
+                yAxis: {
+                    axisLabel: 'Y Axis',
+                    axisLabelDistance: 30
+                }
+            }
+        };
+
+        $scope.data = [
+            {
+                key: "Cumulative Return",
+                values: [
+                    {
+                        "label" : "A" ,
+                        "value" : -29.765957771107
+                    } ,
+                    {
+                        "label" : "B" ,
+                        "value" : 0
+                    } ,
+                    {
+                        "label" : "C" ,
+                        "value" : 32.807804682612
+                    } ,
+                    {
+                        "label" : "D" ,
+                        "value" : 196.45946739256
+                    } ,
+                    {
+                        "label" : "E" ,
+                        "value" : 0.19434030906893
+                    } ,
+                    {
+                        "label" : "F" ,
+                        "value" : -98.079782601442
+                    } ,
+                    {
+                        "label" : "G" ,
+                        "value" : -13.925743130903
+                    } ,
+                    {
+                        "label" : "H" ,
+                        "value" : -5.1387322875705
+                    }
+                ]
+            }
+        ]
 
 	$scope.getBumpers=function(){
 		console.log("Called");
@@ -38,12 +106,31 @@ function mainController($scope, $http){
 					mySum+=data['win'][i];
 
 				}
+				var totalGames=data['win'].length;
 				$scope.playerWins=mySum;
 				$scope.playerLosses=data['win'].length-mySum;
 				console.log("Now")
 				console.log($scope.wins);
 
+				makeGraph([{"label":"Wins","value":mySum},{"label":"Losses","value":totalGames-mySum}]);
+
 			});
 
 	}
+	var makeGraph=function(data){
+		nv.addGraph(function() {
+  var chart = nv.models.pieChart()
+      .x(function(d) { return d.label })
+      .y(function(d) { return d.value })
+      .showLabels(true);
+    d3.select("#chart svg")
+        .datum(data)
+      .transition().duration(1200)
+        .call(chart);
+
+  return chart;
+});
+	}
+
 }
+
